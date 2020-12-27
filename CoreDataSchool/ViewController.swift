fc47c4960e08138ad47914e41115d185d4da55c5
Mykhaylo Levchuk <mykhaylo.levchuk@fieldcode.com>
2020-12-27 20:53:38 +0200

    initial commit

---
+++ CoreDataSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSchool
+//
+//  Created by Mykhaylo Levchuk on 27/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
